f5a030c8fa00a49e04da160acbf6b88732112ea5
Alex Tang <alex.m.tang@gmail.com>
2013-08-11 23:51:24 -0400

    Fix install

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ dir=~/.dotfiles                           # dotfiles directory
 olddir=~/.dotfiles_old                    # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files="bash_profile bashrc gemrc gitconfig inputrc vimrc"
+files="bash_profile bashrc gemrc gitconfig inputrc vim vimrc"
 
 ##########
 
